d1d326d944c287cb4f05c1794ea99dfc22902e0e
ekaterinalojko <eloiko@energocomm.ru>
2025-02-24 01:04:21 +0300

    add new test

--- src/test/java/ApiLoginTest.java
+++ src/test/java/ApiLoginTest.java
@@ -1,11 +1,18 @@
 import io.qameta.allure.*;
+import io.qameta.allure.internal.shadowed.jackson.core.JsonProcessingException;
+import io.qameta.allure.internal.shadowed.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.containsString;
 
 @Epic("API тесты")
@@ -45,5 +52,26 @@ public class ApiLoginTest {
                 .then()
                 .statusCode(200)
                 .header("Content-Type", containsString("text/html"));
+    }
+
+    @Test
+    @DisplayName("Проверка логина")
+    @Description("Этот тест проверяет, что возможен логин")
+    @Severity(SeverityLevel.BLOCKER)
+    public void testLogin() {
+        String body = String.format("{\"login\":\"%s\",\"password\":\"%s\",\"rememberme\":%s}","saperew170@ahaks.com", "8f2bc376", true);
+        Response response = given()
+                .spec(postSpec("https://org.1-ofd.ru/api/cp-core/user", "/login", body))
+                .post();
+        response.then().statusCode(200);
+    }
+
+    public static RequestSpecification postSpec(String url, String basePath, String body) {
+        return new RequestSpecBuilder()
+                .setBody(body)
+                .setBaseUri(url)
+                .setBasePath(basePath)
+                .setContentType(JSON)
+                .build();
     }
 }
