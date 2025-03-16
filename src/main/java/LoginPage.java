54e52196e75592a1aa853984ba202f49f25277a8
ekaterinalojko <eloiko@energocomm.ru>
2025-03-16 11:24:31 +0300

    change Allure implementation

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import io.qameta.allure.*;
 
 public class LoginPage {
 
